775ee30ded5d54c782dc136e3d2bbf0dcf3618a5
su0su <lk234869@naver.com>
2021-10-04 14:58:18 +0900

    수경 : cctv api x좌표

--- map/views.py
+++ map/views.py
@@ -8,12 +8,19 @@ import requests
 def main(request):
     url="http://openapi.seoul.go.kr:8088/6743624b646c6b323433736e6f7647/json/safeOpenCCTV_nw/1/5/"
 
+    cctv_result=[]
     res=requests.get(url)
-    rdata=res.json()
-    print(rdata)
-    
-    context = {
-        'rdata': rdata
-    }
+    rdata=res.json()['safeOpenCCTV_nw']
+    items=rdata.get('row')
+
+    for result in items:
+        context = {
+            'WGSXPT' : result['WGSXPT']
+        }
+        cctv_result.append(context)
+
+    print(cctv_result)
+
+
     return render(request, 'map/main.html', context)
 

ddc6ed3989b505fac2be776775df7590ce7ba85d
wwyyww <yso00302@gmail.com>
2021-10-04 18:12:49 +0900

    update views.py

--- map/views.py
+++ map/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from map.models import Cctv, Violent
+
 
 # Create your views here.
 # def map(request):
@@ -26,15 +28,28 @@ def main(request):
     items=rdata.get('row')
 
     for result in items:
+        # cctv = Cctv()
+        # cctv.latitude = float(result['WGSXPT']) 
+        # cctv.longtitude = float(result['WGSYPT'])
+        # cctv.save()
+
         location = {
             'x' : result['WGSXPT'],
             'y' : result['WGSYPT']
         }
         cctv_result.append(location)
 
+    
+    class_object=Cctv.objects.all()
+
     items = {
-        'location' : cctv_result
+        'location' : cctv_result,
+        'class_object' : class_object
     }
+
+    
+
+    
 
 
     return render(request, 'map/main.html', items)

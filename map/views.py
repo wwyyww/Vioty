ea0f4f9d16b60383d083e74cb5a5c6cbc6e299bd
su0su <lk234869@naver.com>
2021-10-11 14:26:06 +0900

    sue

--- map/views.py
+++ map/views.py
@@ -20,7 +20,7 @@ import json
 #     return render(request, 'main.html', context)
 
 def main(request):
-    url="http://openapi.seoul.go.kr:8088/6743624b646c6b323433736e6f7647/json/safeOpenCCTV_nw/1/1000/"
+    url="http://openapi.seoul.go.kr:8088/6743624b646c6b323433736e6f7647/json/safeOpenCCTV_nw/1001/1810/"
 
     cctv_result=[]
     res=requests.get(url)
@@ -29,10 +29,11 @@ def main(request):
     items=rdata.get('row')
 
     for result in items:
-        # cctv = Cctv()
-        # cctv.latitude = float(result['WGSXPT']) 
-        # cctv.longtitude = float(result['WGSYPT'])
-        # cctv.save()
+        cctv = Cctv()
+        cctv.latitude = float(result['WGSXPT']) 
+        cctv.longtitude = float(result['WGSYPT'])
+        cctv.address = result['ADDR']
+        cctv.save()
 
         location = {
             'x' : result['WGSXPT'],

1154e3ff13fd763a79056d78ae5825c4b877a73a
wwyyww <yso00302@gmail.com>
2021-10-11 14:33:38 +0900

    update views.py
    load cctv data from db

--- map/views.py
+++ map/views.py
@@ -20,39 +20,47 @@ import json
 #     return render(request, 'main.html', context)
 
 def main(request):
-    url="http://openapi.seoul.go.kr:8088/6743624b646c6b323433736e6f7647/json/safeOpenCCTV_nw/1/1000/"
-
+    #db 추가작업
+    # url="http://openapi.seoul.go.kr:8088/6743624b646c6b323433736e6f7647/json/safeOpenCCTV_nw/1/1000/"
     # url="http://openapi.seoul.go.kr:8088/6743624b646c6b323433736e6f7647/json/safeOpenCCTV_nw/1001/1810/"
 
     cctv_result=[]
-    res=requests.get(url)
+    # res=requests.get(url)
 
-    rdata=res.json()['safeOpenCCTV_nw']
-    items=rdata.get('row')
+    # rdata=res.json()['safeOpenCCTV_nw']
+    # items=rdata.get('row')
 
-    for result in items:
-        cctv = Cctv()
-        cctv.latitude = float(result['WGSXPT']) 
-        cctv.longtitude = float(result['WGSYPT'])
-        cctv.address = str(result['ADDR'])
-        cctv.save()
+    # for result in items:
+    #     cctv = Cctv()
+    #     cctv.latitude = float(result['WGSXPT']) 
+    #     cctv.longtitude = float(result['WGSYPT'])
+    #     cctv.address = str(result['ADDR'])
+    #     cctv.save()
 
-        location = {
-            'x' : result['WGSXPT'],
-            'y' : result['WGSYPT']
-        }
-        cctv_result.append(location)
+    #     location = {
+    #         'x' : result['WGSXPT'],
+    #         'y' : result['WGSYPT']
+    #     }
+    #     cctv_result.append(location)
 
     
     class_object=Cctv.objects.all()
-    
 
-    data=json.dumps(cctv_result)
+    for object in class_object:
+        print("check : ", object.latitude)
+        location = {
+            'x' : object.latitude,
+            'y' : object.longtitude
+        }
+        cctv_result.append(location)
 
     items = {
-        'location' : cctv_result,
-        'data' : data
+        'location' : cctv_result
     }
+
+    # data=json.dumps(cctv_result)
+
+
 
 
 

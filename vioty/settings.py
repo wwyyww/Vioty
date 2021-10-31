725895a5822c06846879b96dbd04f1a7f37405ee
leesooin0422 <78577772+leesooin0422@users.noreply.github.com>
2021-10-31 16:51:42 +0900

    commit
    index

--- vioty/settings.py
+++ vioty/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'vioty',
         'USER':'root',
-        'PASSWORD':'autoset',
+        'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306'
     }
